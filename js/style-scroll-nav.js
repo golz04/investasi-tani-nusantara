cee203c8d42e0ae721aff19ada1d50236f440207
Muhammad Haidar <muhammadhaidar862@gmail.com>
2021-04-04 19:52:35 +0700

    90%

---
+++ js/style-scroll-nav.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= 200){
+            $(".navbar").css({
+                "background-color" : "white",
+                "box-shadow" : "0 3px rgba(156, 156, 156, 0.5)",
+                "transition" : ".5s"
+            });
+            $(".navbar a").css("color", "black");
+            $(".nav-dropdown .btn-dropdown").css("color", "black");
+        } else {
+            $(".navbar").css({
+                "background-color" : "transparent",
+                "box-shadow" : "0 3px rgba(156, 156, 156, 0)",
+                "transition" : ".5s"
+            });
+            $(".navbar a").css("color", "white");
+            $(".dropdown-menu a").css("color", "black");
+        }
+    });
+});
---
+++ js/style-slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+});
